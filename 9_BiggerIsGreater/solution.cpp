209e379b1e9660cb1961ac8d52e49edc864a8e70
TomoyaTatara <39728102+TomoyaTatara@users.noreply.github.com>
2022-10-21 19:45:24 +0100

    Create solution.cpp
    solved by Tatara

---
+++ 9_BiggerIsGreater/solution.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'biggerIsGreater' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING w as parameter.
+ */
+
+string biggerIsGreater(string w) {
+    if(w.length() == 1) {
+        return "no answer";
+    }
+    
+    int i, j;
+
+    for(i=w.length()-1; i>0; i--) {
+        if(w[i] > w[i-1]) {
+            for(j=i; j<w.length(); j++) {
+                if(w[i-1] >= w[j]) {
+                    break;
+                }
+            }
+            char tmp = w[j-1];
+            w[j-1] = w[i-1];
+            w[i-1] = tmp;
+            break;
+        }
+    }
+
+    if(i != 0) {
+        sort(w.begin()+i, w.end());
+        return w;
+    }
+
+    return "no answer";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string T_temp;
+    getline(cin, T_temp);
+
+    int T = stoi(ltrim(rtrim(T_temp)));
+
+    for (int T_itr = 0; T_itr < T; T_itr++) {
+        string w;
+        getline(cin, w);
+
+        string result = biggerIsGreater(w);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
